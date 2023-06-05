3e04c651effb3acfdddcb506b247c70702a6036a
kingmuffy <65783231+kingmuffy@users.noreply.github.com>
2023-06-05 23:16:11 +0100

    register

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -13,7 +13,7 @@ const RegisterScreen = () => {
           <div>
             <label
               htmlFor="name"
-              className="block text-sm font-medium leading-6 text-gray-900">
+              className="block text-sm font-medium leading-6 text-center text-gray-900">
               Name
             </label>
             <div className="mt-2">
@@ -30,7 +30,7 @@ const RegisterScreen = () => {
           <div>
             <label
               htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900">
+              className="block text-sm font-medium leading-6 text-center text-gray-900">
               Email address
             </label>
             <div className="mt-2">
@@ -45,22 +45,9 @@ const RegisterScreen = () => {
             </div>
           </div>
           <div>
-            <label className="relative cursor-pointer block text-sm font-medium leading-6 text-gray-900">
-              <input
-                type="text"
-                placeholder="input"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2 border-opacity-50 outline-none focus:border-blue-500 placeholder-gray-300 placeholder-opacity-0 transition duration-200"
-              />
-              <span className="text-sm text-white text-opacity-80 absolute left-4 top-2 px-1 transition duration-200 input-text">
-                Input
-              </span>
-            </label>
-          </div>
-
-          <div>
             <label
               htmlFor="password"
-              className="block text-sm font-small leading-6 text-gray-900">
+              className="block text-sm font-small leading-6 text-center text-gray-900">
               Password
             </label>
             <div className="mt-2">
@@ -90,6 +77,13 @@ const RegisterScreen = () => {
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"
               />
             </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              Sign in
+            </button>
           </div>
         </form>
       </div>
