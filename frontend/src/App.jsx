ac7fb47a82658d85246cbf607818e9879c0431a7
kingmuffy <65783231+kingmuffy@users.noreply.github.com>
2023-06-05 12:58:50 +0100

    sign in

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import LoginScreen from './screens/LoginScreen'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-white">Hello World</h1>
+      <LoginScreen />
+    </div>
+  )
+}
+
+export default App
