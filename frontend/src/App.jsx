efa9ee9b439549190e0a8c92e78db601f3968559
kingmuffy <65783231+kingmuffy@users.noreply.github.com>
2023-06-06 09:38:31 +0100

    route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,6 @@
+import Loader from './Loader'
+import { Outlet } from 'react-router-dom'
+
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 
@@ -6,7 +9,9 @@ function App() {
     <div>
       <h1 className="text-white">Hello World</h1>
 
-      <RegisterScreen />
+      <div class="container mx-auto">
+        <Outlet />
+      </div>
     </div>
   )
 }
